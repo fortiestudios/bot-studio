fbccd331fe3b740a6b806b8bf99db1ba66b68e8c
vicvenpet <74773810+vicvenpet@users.noreply.github.com>
2022-08-05 17:52:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//           Installed with MIT License
+//
+//           Discord Support: https://discord.gg/wDNqXZ7wpq
+
+const fs = require("fs");
+
+const yaml = require("js-yaml");
+const supportbot = yaml.load(
+  fs.readFileSync("./Configs/supportbot.yml", "utf8")
+);
+
+const Client = require("./Structures/Client.js");
+const client = new Client({});
+
+client.start(supportbot.Token);
